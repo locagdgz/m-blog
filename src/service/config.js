56b3a91fc0b7f1c8f2793ac5084092fa4efa16bc
locadesktop <locagdgz@gmail.com>
2021-02-23 03:05:39 +0800

    none

--- src/service/config.js
+++ src/service/config.js
@@ -1,7 +1,5 @@
-// const devBaseURL = "http://localhost:8080/blog"
-const proBaseURL = "http://106.52.156.191:8080/blog"
-const devBaseURL = "http://106.52.156.191:8080/blog"
-// const proBaseURL = "https://production.org"
+const devBaseURL = "http://localhost:8080/blog"
+const proBaseURL = "https://production.org"
 
 export const BASE_URL = process.env.NODE_ENV === 'development' ? devBaseURL : proBaseURL
 
